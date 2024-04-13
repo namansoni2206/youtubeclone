86a84fbd02322c3faf4d391787dcb336af6841a7
soninaman2206 <soninaman2206@gmail.com>
2024-04-13 10:16:10 +0530

    database connection

---
+++ src/index.js
@@ -0,0 +1,41 @@
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
+import express from "express";
+const app = express();
+
+dotenv.config({
+    path : './env'
+});
+
+// require('dotenv').config({path:'./env'});
+
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
+
+
+
+
+
+
+/*
+
+
+;(async()=>{
+    try {
+        await mongoose.connect('${process.env.MONGODB_URI}/${DB_NAME}')
+        app.on("error",(error)=>{
+            console.log("ERROR : can not connect to database")
+        })
+    } catch (error) {
+        console.error("ERROR" , error)
+    }
+})()
+*/
