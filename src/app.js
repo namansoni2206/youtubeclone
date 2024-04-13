7e28c496a15594814812b8f27de47a5230969484
soninaman2206 <soninaman2206@gmail.com>
2024-04-13 11:05:53 +0530

    util code

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+
+const app = express()
+
+app.use(cors({
+    origin : process.env.CORS_ORIGIN,
+    credentials : true
+}))
+
+
+// json data handle
+app.use(express.json({limit : "16kb"}))
+
+// url handling as url is different for different browsers   /  extended : true - objects ke andr objects bhi rakh sakte hai
+app.use(express.urlencoded({extended : true , limit : "16kb"}))
+
+
+//in case agr koi image file rakhni ho
+app.use(express.static("public"))
+
+
+//handling cookies
+app.use(cookieParser())
+
+export {app}
