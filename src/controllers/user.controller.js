dc77fac8b0696b99ce35a59097548bc630c5ab83
soninaman2206 <soninaman2206@gmail.com>
2024-04-23 00:04:45 +0530

    Aggregate Pipeline

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -280,7 +280,7 @@ const updateAccountDetails = asyncHandler(async(req,res)=>{
     throw new ApiError(400,"All fields are required")
   }
 
-  const user = User.findByIdAndUpdate(req.user._id,
+  const user = await User.findByIdAndUpdate(req.user._id,
   {
     $set:{
       fullName : fullName,
@@ -354,4 +354,76 @@ const updateUserCoverImage = asyncHandler(async(res,req)=>{
 })
 
 
-export { registerUser , loginUser , logoutUser , refreshAccessToken , ChangeCurrentPassword , getCurrentUser , updateAccountDetails , updateUserAvatar , updateUserCoverImage};
+// Aggregation Pipeline 
+const getUserChannelProfile = asyncHandler(async(res,req)=>{
+    const {username} = req.params
+
+    if(!username?.trim()){
+      throw new ApiError(400,"username is missing")
+    }
+
+    //  aggregate - return array 
+    const channel = await User.aggregate([
+      {
+        $match:{
+          username : username?.toLowerCase()
+        }
+      },
+      {
+        $lookup:{
+          from : "subscriptions",
+          localField : "_id",
+          foreignField : "channel",
+          as : "subscribers"
+        }
+      },
+      {
+        $lookup : {
+          from : "subscriptions",
+          localField : "_id",
+          foreignField : "subscriber",
+          as : "subscribedTo"
+        }
+      },
+      {
+        $addFields :{
+          subscribersCount : {
+            $size : "$subscribers"     // use $ as subscribers is field
+          },
+          subscribedToCount : {
+            $size : "$subscribedTo"
+          },
+          isSubscribed : {
+            $cond : {
+              if : {$in : [req.user?._id , "$subscribers.subscriber"]},
+                then : true,
+                else : false
+            }
+           }
+        }
+      },
+      {
+        $project : {
+          fullName : 1,
+          username : 1,
+          subscribersCount : 1,
+          subscribedToCount : 1,
+          isSubscribed : 1,
+          avatar : 1,
+          coverImage : 1,
+          email : 1
+        }
+      }
+    ])    
+    
+    if(!channel?.length){
+      throw new ApiError(404,"Channel does not exists")
+    }
+
+    return res.status(200).json(
+      new ApiResponse(200,channel[0],"User channel fetched successfully")
+    )
+})
+ 
+
+export { registerUser , loginUser , logoutUser , refreshAccessToken , ChangeCurrentPassword , getCurrentUser , updateAccountDetails , updateUserAvatar , updateUserCoverImage , getUserChannelProfile};
--- src/models/subscription.models.js
+++ src/models/subscription.models.js
@@ -1,5 +1,9 @@
 import mongoose,{ Schema } from "mongoose";
 
+// to find the subscribers of a channel , we will find the count of docs where channel is the id of the channel (unique doc will be maintained for each channel-subscriber relation)
+
+// to find a user subscribed to how many channels - just select a subscriber with that user and then count the channel number  
+
 const subscriptionSchema = new Schema ({
     subscriber : {
         type : Schema.Types.ObjectId ,     // one who is subscribing
