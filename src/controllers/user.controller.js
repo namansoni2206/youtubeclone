fa986ccefd69d7b69e586308e3eaa0be0dab944d
soninaman2206 <soninaman2206@gmail.com>
2024-04-23 00:37:12 +0530

    Watch History Pipeline

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -426,4 +426,55 @@ const getUserChannelProfile = asyncHandler(async(res,req)=>{
 })
  
 
-export { registerUser , loginUser , logoutUser , refreshAccessToken , ChangeCurrentPassword , getCurrentUser , updateAccountDetails , updateUserAvatar , updateUserCoverImage , getUserChannelProfile};
+const getWatchHistory = asyncHandler(async(req,res)=>{
+  const user = await User.aggregate([
+    {
+      $match : {
+        _id : new mongoose.Types.ObjectId(req.user?._id)
+      }
+    },
+    {
+      $lookup:{
+        from : "videos",
+        localField : "watchHistory",
+        foreignField : "_id",
+        as : "watchHistory",
+        pipeline : [
+          {
+            $lookup :{
+              from : "users",
+              localField : "owner",
+              foreignField : "_id",
+              as: "owner",
+              pipeline :[
+                {
+                  $project : {
+                    fullName : 1,
+                    username : 1,
+                    avatar : 1
+                  }
+                }
+              ]
+            }
+        },
+        {
+          $addFields : {
+            owner : {
+              $arrayElemAt : ["$owner",0]
+            }
+          }
+        }
+      ]}
+    }
+    
+  ])
+
+  return res.status(200).json(
+    new ApiResponse(200,user[0].getWatchHistory,"Watch History Fetched Successfully ")
+  )
+})
+
+
+
+
+export { registerUser , loginUser , logoutUser , refreshAccessToken , ChangeCurrentPassword , getCurrentUser , updateAccountDetails , updateUserAvatar , updateUserCoverImage , getUserChannelProfile , getWatchHistory}
